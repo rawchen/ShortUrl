8f3788a0a6b06ab9f9a2f53b8bde8821d7df721d
RawChen <2221999792@qq.com>
2022-07-28 00:38:16 +0800

    fix: 前端优化

--- src/main/resources/static/js/application.js
+++ src/main/resources/static/js/application.js
@@ -1,36 +1,38 @@
-const input = document.getElementById("longurl");
-input.focus();
+$(function() {
 
-$('#longurl').bind("keypress", function() {
-    if (event.keyCode === 13) {
-        if (!$('#shortenurl').prop("disabled")) {
-            doSubmit();
+    $('#longurl').bind("keypress", function() {
+        if (event.keyCode === 13) {
+            if (!$('#shortenurl').prop("disabled")) {
+                doSubmit();
+            }
         }
-    }
-});
-
-if($('[data-toggle="datepicker"]').length > 0){
-    $('[data-toggle="datepicker"]').datepicker({
-        autoPick: false,
-        autoHide: true,
-        format: "yyyy/mm/dd",
-        days:["星期日","星期一","星期二","星期三","星期四","星期五","星期六"],
-        daysShort:["周日","周一","周二","周三","周四","周五","周六"],
-        daysMin:["日","一","二","三","四","五","六"],
-        months:["一月","二月","三月","四月","五月","六月","七月","八月","九月","十月","十一月","十二月"],
-        monthsShort:["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"],
-        startDate: new Date()
     });
-}
 
-$(window).scroll(function(){
-    if(window.pageYOffset>300){
-        $("#backtop,#back-to-top").fadeIn('slow');
-    }else{
-        $("#backtop,#back-to-top").fadeOut('slow');
+    if($('[data-toggle="datepicker"]').length > 0){
+        $('[data-toggle="datepicker"]').datepicker({
+            autoPick: false,
+            autoHide: true,
+            format: "yyyy/mm/dd",
+            days:["星期日","星期一","星期二","星期三","星期四","星期五","星期六"],
+            daysShort:["周日","周一","周二","周三","周四","周五","周六"],
+            daysMin:["日","一","二","三","四","五","六"],
+            months:["一月","二月","三月","四月","五月","六月","七月","八月","九月","十月","十一月","十二月"],
+            monthsShort:["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"],
+            startDate: new Date()
+        });
     }
+
+    $(window).scroll(function(){
+        if(window.pageYOffset>300){
+            $("#backtop,#back-to-top").fadeIn('slow');
+        }else{
+            $("#backtop,#back-to-top").fadeOut('slow');
+        }
+    });
+    $("a#back-to-top").smoothscroll();
+
+    document.getElementById("longurl").focus();
 });
-$("a#back-to-top").smoothscroll();
 
 function isUrl(str) {
     const v = new RegExp('^(?!mailto:)(?:(?:http|https|ftp)://|//)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$', 'i');
@@ -51,7 +53,7 @@ function doSubmit() {
 
         // 如果包含本域名
         if (link.indexOf(window.location.host) !== -1) {
-            input.focus();
+            document.getElementById("longurl").focus();
             layer.msg('不允许循环哦', {icon: 2,closeBtn: 2});
             return;
         }
@@ -100,7 +102,7 @@ function doSubmit() {
             }
         });
     } else {
-        input.focus();
+        document.getElementById("longurl").focus();
         layer.msg('URL格式不正确', {icon: 2,closeBtn: 2});
     }
 }
