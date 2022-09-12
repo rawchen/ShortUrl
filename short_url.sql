8ba5b56b85a2007924d396647d2fc28435d709f7
RawChen <2221999792@qq.com>
2022-09-13 00:10:07 +0800

    fix: sql

--- short_url.sql
+++ short_url.sql
@@ -4,18 +4,35 @@
  Source Server         : short_url
  Source Server Type    : MySQL
  Source Server Version : 50734
- Source Host           : 127.0.0.1:3306
  Source Schema         : short_url
 
  Target Server Type    : MySQL
  Target Server Version : 50734
  File Encoding         : 65001
 
- Date: 12/08/2022 22:35:32
+ Date: 13/09/2022 00:08:25
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for log
+-- ----------------------------
+DROP TABLE IF EXISTS `log`;
+CREATE TABLE `log`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `ua` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT 'UserAgent',
+  `browser_name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '浏览器名',
+  `os_name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '操作系统名',
+  `api_path` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '访问api路径',
+  `ip` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT 'ip地址',
+  `referer` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '来源地址',
+  `access_time` datetime(0) NULL DEFAULT NULL COMMENT '访问时间',
+  `short_url_id` bigint(20) UNSIGNED NULL DEFAULT NULL COMMENT '短链id',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_short_url_id`(`short_url_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = COMPACT;
 
 -- ----------------------------
 -- Table structure for short_url
@@ -27,6 +44,9 @@ CREATE TABLE `short_url`  (
   `link` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
   `effective_date` date NULL DEFAULT NULL,
   `password` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `ip` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `save_time` datetime(0) NULL DEFAULT NULL,
+  `view_number` bigint(20) NULL DEFAULT 0,
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `code`(`code`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
